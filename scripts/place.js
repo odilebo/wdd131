a81017fb89c796e348226eba709cb3797d062b49
oarabiledilebo <oarabiledilebo@gmail.com>
2025-02-08 22:24:30 +0200

    week3 project

---
+++ scripts/place.js
@@ -0,0 +1,36 @@
+const full = document.querySelector("#currentyear");
+const day = document.querySelector("#lastModified");
+
+// use the date object
+const today = new Date();
+let lastMod = new Date(document.lastModified);
+
+full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+
+day.innerHTML = `Last Modified:  <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(lastMod)}</span>`;
+
+full.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+const temp = 15
+const wind = 10
+
+function windFactor( temp, wind) {
+    if (temp <= 10 && wind > 4.8) {
+       return ` Wind Factor: ${wind} km/h  `;
+    } else {
+        return ` Wind Factor:  n/a `;
+    }
+
+  }
+
+document.querySelector('#temp').innerHTML = windFactor(temp, wind);
