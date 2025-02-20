8a3a75ddebdda766efe84add538b208515498679
oarabiledilebo <oarabiledilebo@gmail.com>
2025-02-21 00:10:33 +0200

    new changes

--- scripts/form.js
+++ scripts/form.js
@@ -1,11 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     const productSelect = document.getElementById("product");
     const products = [
-        { id: "idea1", name: "Electric Light Bulb" },
-        { id: "idea2", name: "Internet" },
-        { id: "idea3", name: "Smartphone" },
-        { id: "idea4", name: "Printing Press" }
-    ];
+        {
+          id: "fc-1888",
+          name: "flux capacitor",
+          averagerating: 4.5
+        },
+        {
+          id: "fc-2050",
+          name: "power laces",
+          averagerating: 4.7
+        },
+        {
+          id: "fs-1987",
+          name: "time circuits",
+          averagerating: 3.5
+        },
+        {
+          id: "ac-2000",
+          name: "low voltage reactor",
+          averagerating: 3.9
+        },
+        {
+          id: "jj-1969",
+          name: "warp equalizer",
+          averagerating: 5.0
+        }
+      ];
 
     products.forEach(product => {
         const option = document.createElement("option");
